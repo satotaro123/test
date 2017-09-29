13e8ab8aec83cef0a1daefa66d6867dd31e23aea
sakazawasaburo <keitaro_nishizawa@gyosei-system.co.jp>
2017-09-29 16:03:38 +0900

    login

---
+++ src/main/java/com/example/SecurityConfig.java
@@ -0,0 +1,108 @@
+
+package com.example;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.authentication.encoding.BasePasswordEncoder;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
+@Controller
+@SpringBootApplication
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Autowired
+	@Qualifier("dataSource")
+	private DataSource dataSource;
+
+
+	private static final String USER_QUERY = "SELECT custid, password,role FROM userdata WHERE custid = ?";
+	private static final String ROLE_QUERY = "SELECT CUSTID, reserve FROM userdata WHERE CUSTID=?";
+
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.usersByUsernameQuery(USER_QUERY)
+		.authoritiesByUsernameQuery(ROLE_QUERY);
+	}
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.antMatchers("/login").permitAll()
+		.antMatchers("/**").hasAnyAuthority("USER","ADMIN")
+		.antMatchers("/login/**").hasAnyAuthority("USER","ADMIN")
+		.antMatchers("/Home/**").hasAnyAuthority("USER","ADMIN")
+		.antMatchers("/index/**").hasAuthority("ADMIN")
+		.and()
+		.formLogin()
+		.loginPage("/login");
+		http.formLogin()
+		.defaultSuccessUrl("/Home", true)
+		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/")
+		.deleteCookies("JSESSIONID")
+		.invalidateHttpSession(true).permitAll()
+		.and()
+		.csrf()
+		.disable();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.inMemoryAuthentication()
+		.withUser("user").password("pass").roles("USER").and()
+		.withUser("admin").password("pass").roles("ADMIN");
+	}
+
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		if (dbUrl == null || dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
+	}
+}
+
